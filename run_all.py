3b60ed32f989ce9e0a6e6c9b4a767a4be409b6a7
Shadrak Bsh <142589989+Bsh54@users.noreply.github.com>
2024-12-23 06:01:25 -0800

    Add files via upload

---
+++ run_all.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.Popen(['python3', 'script1.py'])
+subprocess.Popen(['python3', 'script2.py'])
+subprocess.Popen(['python3', 'script3.py'])
+subprocess.Popen(['python3', 'script4.py'])
